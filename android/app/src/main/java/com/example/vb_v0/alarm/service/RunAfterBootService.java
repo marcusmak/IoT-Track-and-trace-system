05a24161e9710f625f12ca9cde7c206d232dd0c9
marcusmak <mak.marcus8@gmail.com>
2020-12-17 14:31:28 +0000

    first onboot try

---
+++ android/app/src/main/java/com/example/vb_v0/alarm/service/RunAfterBootService.java
@@ -0,0 +1,45 @@
+package com.example.vb_v0.alarm.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+import android.util.Log;
+
+public class RunAfterBootService extends Service {
+
+    private static final String TAG_BOOT_EXECUTE_SERVICE = "BOOT_BROADCAST_SERVICE";
+
+    public RunAfterBootService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG_BOOT_EXECUTE_SERVICE, "RunAfterBootService onCreate() method.");
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        String message = "RunAfterBootService onStartCommand() method.";
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+        Log.d(TAG_BOOT_EXECUTE_SERVICE, "RunAfterBootService onStartCommand() method.");
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
